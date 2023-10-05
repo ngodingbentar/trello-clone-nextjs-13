8194fbad09c63104f515998184478b4f6f79a16d
ngodingbentar <dewaqintoro@gmail.com>
2023-10-05 15:59:02 +0700

    added: header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,29 @@
-'use client'
-
-import Link from "next/link"
 import './header.css'
 import Image from "next/image"
-import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid"
+import { MagnifyingGlassCircleIcon, UserCircleIcon } from "@heroicons/react/24/solid"
 
 function Header () {
   return (
     <header className="header">
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-2xl">
-        <Image src="https://picsum.photos/300/100" alt="my-image" width={300} height={100} className="w-44 md:w-56 pb-10 md:pb-0 object-contain" />
-        <div>
+        <Image src="https://picsum.photos/50/50" alt="my-image" width={50} height={50} className="pb-10 md:pb-0 object-contain" />
+
+        <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassCircleIcon className="h-6 w-6 text-gray-400" />
             <input type="text" placeholder="Search..." className="flex-1 outline-none p-2" />
             <button type="submit" hidden>Search</button>
           </form>
+          <Image src="https://picsum.photos/50/50" alt="my-image" width={50} height={50} className="object-contain rounded-full " />
         </div>
+
+      </div>
+
+      <div className='flex items-center justify-center px-5 md:py-5'>
+        <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-blue-400'>
+          <UserCircleIcon className='inline-block h-10 w-10 text-blue-600 mr-1 ' />
+          I'am GPT...
+        </p>
       </div>
     </header>
   )
