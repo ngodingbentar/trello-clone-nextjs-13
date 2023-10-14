6bf8bfb8d218a200efa6b6e45e5bf069665b3eb0
ngodingbentar <dewaqintoro@gmail.com>
2023-10-14 16:19:39 +0700

    added: vercel.json

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   images: {
     domains: ['picsum.photos', 'links.papareact.com', 'cloud.appwrite.io']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://cloud.appwrite.io/v1/:path*',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
