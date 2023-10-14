41c6bfee85a7df475a71f6785737f2756a4bad45
ngodingbentar <dewaqintoro@gmail.com>
2023-10-14 15:52:42 +0700

    added: function for delete board

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useBoardStore } from "@/store/BoardStore";
 import { Todo, TypedColumn } from "@/typings";
 import { XCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
@@ -25,6 +26,9 @@ function TodoCard({
   draggableProps,
   dragHandleProps,
 }: Props) {
+
+  const deleteTask = useBoardStore((state) => state.deleteTask);
+
   return (
     <div
       className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -36,6 +40,7 @@ function TodoCard({
         <p className="font-normal">{todo.title}</p>
         <button
           className="text-red-500 hover:text-red-600"
+          onClick={() => deleteTask(index, todo, id)}
         >
           <XCircleIcon className="ml-5 h-8 w-8"></XCircleIcon>
         </button>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,4 +1,4 @@
-import { databases } from '@/appwrite';
+import { databases, storage } from '@/appwrite';
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn'
 import { Board, Column, Todo, TypedColumn } from '@/typings'
 import { create } from 'zustand'
@@ -11,6 +11,8 @@ interface BoardState {
 
   searchString: string,
   setSearchString: (searchString: string) => void,
+
+  deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void,
 }
 
 export const useBoardStore = create<BoardState>((set, get) => ({
@@ -36,4 +38,24 @@ export const useBoardStore = create<BoardState>((set, get) => ({
 
   searchString: "",
   setSearchString: (searchString) => set({ searchString }),
+
+  deleteTask: async (taskIndex, todo, id) => {
+    const newColumns = new Map(get().board.columns)
+
+    // delete todoId from newColumns
+    // do it in an optimistic way
+    newColumns.get(id)?.todos.splice(taskIndex, 1)
+
+    set({ board: { columns: newColumns } })
+
+    if (todo.image) {
+        await storage.deleteFile(todo.image.bucketId, todo.image.fileId)
+    }
+
+    await databases.deleteDocument(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!,
+        todo.$id,
+    )
+},
 }))
